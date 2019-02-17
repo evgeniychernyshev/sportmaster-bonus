12059a6c07577c083ac007c01d701f66e0df8403
evgeniych <work.evgeniych@gmail.com>
2019-02-17 19:08:22 +0300

    Tests added

--- app/test/test_bonus.py
+++ app/test/test_bonus.py
@@ -1,7 +1,26 @@
 from app.bonus import bonus
 
 
-def test_bonus_less_min_sum(current_purchase, previous_purchases):
+def test_bonus_less_min_sum():
     result = bonus(500, [199, 599, 1699])
 
     assert 0 == result
+
+
+def test_bonus_blue_card():
+    result = bonus(1399, [399, 899, 8499])
+
+    assert 50 == result
+
+
+def test_bouns_silver_card():
+    result = bonus(2599, [9000, 8990, 15499])
+
+    assert 140 == result
+
+
+def test_bonus_gold_card():
+    result = bonus(3200, [25000, 100000, 25000, 25000])
+
+    assert 300 == result
+
