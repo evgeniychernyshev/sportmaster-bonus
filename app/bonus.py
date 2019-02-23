1cbb51e33fb6a577f32b6388e8e4d0ac32f49e35
evgeniych <work.evgeniych@gmail.com>
2019-02-23 15:36:05 +0300

    Fixed #1

--- app/bonus.py
+++ app/bonus.py
@@ -1,10 +1,10 @@
 def bonus(current_purchase, previous_purchases):
-    blue_card_bonus = 50 # бонусов за каждые полные 1000 руб по синей карте
-    silver_card_bonus = 70 # бонусов за каждые полные 1000 руб по серебряной карте
-    gold_card_bonus = 100 # бонусов за каждые полные 1000 руб по золотой карте
-    total_sum = 0 # сумма всех предыдущих покупок
-    bonus_count = 0 # кол-во бонусов с текущей покупки
-    min_purchase_sum = 1000 # минимальная сумма, на которую начисляются бонусы
+    blue_card_bonus = 50  # бонусов за каждые полные 1000 руб по синей карте
+    silver_card_bonus = 70  # бонусов за каждые полные 1000 руб по серебряной карте
+    gold_card_bonus = 100  # бонусов за каждые полные 1000 руб по золотой карте
+    total_sum = 0  # сумма всех предыдущих покупок
+    bonus_count = 0  # кол-во бонусов с текущей покупки
+    min_purchase_sum = 1_000  # минимальная сумма, на которую начисляются бонусы
 
     for purchase in previous_purchases:
         total_sum += purchase
@@ -12,13 +12,13 @@ def bonus(current_purchase, previous_purchases):
     if current_purchase < min_purchase_sum:
         return 0
 
-    if 1 <= total_sum <= 15000:
+    if 1 <= total_sum <= 15_000:
         bonus_count = current_purchase // min_purchase_sum * blue_card_bonus
 
-    if 15000 < total_sum <= 150000:
+    if 15_000 < total_sum <= 150_000:
         bonus_count = current_purchase // min_purchase_sum * silver_card_bonus
 
-    if total_sum > 150000:
+    if total_sum > 150_000:
         bonus_count = current_purchase // min_purchase_sum * gold_card_bonus
 
     return bonus_count
