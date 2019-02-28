e601932fa8bc117cebe56407815c42123f041191
evgeniych <work.evgeniych@gmail.com>
2019-02-28 21:49:00 +0300

    Fixed #2

--- app/bonus.py
+++ app/bonus.py
@@ -4,7 +4,10 @@ def bonus(current_purchase, previous_purchases):
     gold_card_bonus = 100  # бонусов за каждые полные 1000 руб по золотой карте
     total_sum = 0  # сумма всех предыдущих покупок
     bonus_count = 0  # кол-во бонусов с текущей покупки
-    min_purchase_sum = 1_000  # минимальная сумма, на которую начисляются бонусы
+    min_purchase_sum = 1_000  # минимальная сумма покупки, на которую начисляются бонусы
+
+    # количество полных тысяч в сумме текущей покупки
+    full_thousand_count = current_purchase // min_purchase_sum
 
     for purchase in previous_purchases:
         total_sum += purchase
@@ -13,13 +16,13 @@ def bonus(current_purchase, previous_purchases):
         return 0
 
     if 1 <= total_sum <= 15_000:
-        bonus_count = current_purchase // min_purchase_sum * blue_card_bonus
+        bonus_count = full_thousand_count * blue_card_bonus
 
     if 15_000 < total_sum <= 150_000:
-        bonus_count = current_purchase // min_purchase_sum * silver_card_bonus
+        bonus_count = full_thousand_count * silver_card_bonus
 
     if total_sum > 150_000:
-        bonus_count = current_purchase // min_purchase_sum * gold_card_bonus
+        bonus_count = full_thousand_count * gold_card_bonus
 
     return bonus_count
 
